8733fc0449bc01c0964165f351005ca10e59e0ed
James Hall <rio@x5g.com>
2010-09-08 20:59:37 +0100

    Fixes, options panel

--- highrise-vonage.js
+++ highrise-vonage.js
@@ -6,25 +6,34 @@ for(var el in contact_numbers) {
 	var contact = contact_numbers[el].innerHTML;
 	if(contact != undefined) {
 		var parts = contact_numbers[el].innerHTML.split(/<span/);
-		var number = parts[0].replace(/^\s+|\s+$/, '');
+		var number = parts[0].replace(/^\s+|\s+$/, '').replace('+', '00');
 		if(number.indexOf('href') == -1) {
-			var phone_icon = ' <a href="#" data-number="' + number + '" id="vonage_hook_' + i + '" style="background-color: transparent"><img src="chrome-extension://lgjnmiemnghmdpkhkfdgmcablokenkph/phone.png" /></a>';
+			var phone_icon = ' <a href="#" data-number="' + number + '" id="vonage_hook_' + i + '" style="background-color: transparent"><img src="' + chrome.extension.getURL("phone.png") + '" /></a>';
 			contact_numbers[el].innerHTML = number + phone_icon + '<span' + parts[1];
 			document.getElementById('vonage_hook_' + i).addEventListener("click", function(e) {
 				var number = e.srcElement.parentElement.getAttribute('data-number');
-				if(confirm('Are you sure you wish to call ' + number + '?')) {
-
-					
-					chrome.extension.sendRequest({'action': 'getSettings'}, function(settings) {
-						var url = 'https://secure.click2callu.com/tpcc/makecall?username= ' + settings.username + '&password=' + settings.password + '&fromnumber=441904236363&tonumber=' + number;
-						console.log(url);
+				chrome.extension.sendRequest({'action': 'getSettings'}, function(settings) {					
+					if(confirm('Are you sure you wish to call ' + number + '?')) {						
+						// Get from numbers
+						var url = 'https://secure.click2callu.com/tpcc/getnumbers?username='  + settings.username + '&password=' + settings.password;
 						chrome.extension.sendRequest({'action' : 'doAjaxRequest', 'url' : url}, function(data) {
-							// Callback
-						});		
-					});
+							var from_numbers = data.split(/,/);
+							var url = 'https://secure.click2callu.com/tpcc/makecall?username=' + settings.username + '&password=' + settings.password + '&fromnumber=' + from_numbers[0] + '&tonumber=' + number;
+							//console.log(url);
+							chrome.extension.sendRequest({'action' : 'doAjaxRequest', 'url' : url}, function(data) {
+								// Callback
+								console.log(data);
+								if(data == '100:Authentication Failed') {
+									alert('Vonrise\n\nSorry, your username and password for Vonage was incorrect. Please check your settings.')
+								}
+							});								
+							
+						});
+							
+					};
 					
 			
-				}
+				});
 			});
 		}
 	}
